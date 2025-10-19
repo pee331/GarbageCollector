3fda0fb353897c90428765f3d8bf95691ca4dc1e
Peny <43066291+pee331@users.noreply.github.com>
2025-10-19 18:18:14 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/garbagecollector/main.go
@@ -0,0 +1,23 @@
+// cmd/garbagecollector/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "garbagecollector/internal/garbagecollector"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := garbagecollector.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
